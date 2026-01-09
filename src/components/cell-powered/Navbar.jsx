d84dc7327a72559a12157c553d9bf7e926916e01
noahgree <ngreene2026@gmail.com>
2026-01-09 17:42:39 -0500

    New grain style

--- src/components/cell-powered/Navbar.jsx
+++ src/components/cell-powered/Navbar.jsx
@@ -4,7 +4,7 @@ import { AppleIcon, DownloadIcon } from "../../Reusable/svgs";
 export default function Navbar() {
    return (
       <div className="flex fixed top-0 left-0 right-0 z-50 justify-center max-w-4xl mx-auto">
-         <div className="navbar backdrop-blur-2xl flex-nowrap rounded-4xl overflow-hidden w-[80vw] min-w-60 my-8 grain-bg border border-white/20">
+         <div className="navbar backdrop-blur-2xl flex-nowrap rounded-4xl overflow-hidden w-[80vw] min-w-60 my-8 bg-white/5 border border-white/10">
             <div className="navbar-start z-1">
                <a
                href="#top"
@@ -14,11 +14,11 @@ export default function Navbar() {
                </a>
             </div>
             <div className="navbar-end mx-1">
-               <a className="btn rounded-4xl btn-soft glass hover:bg-green-400/10 hidden sm:inline-flex">
+               <a className="btn rounded-4xl btn-soft glass hover:bg-green-400/10 hidden sm:inline-flex backdrop-blur-lg">
                   <AppleIcon size={18}/>
                   Download Now
                </a>
-               <a className="btn btn-circle btn-soft glass hover:bg-green-400/10 sm:hidden">
+               <a className="btn btn-circle btn-soft glass hover:bg-green-400/10 sm:hidden backdrop-blur-lg">
                   <DownloadIcon size={18}/>
                </a>
             </div>
--- src/pages/CellPowered.jsx
+++ src/pages/CellPowered.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/cell-powered/Hero"
 
 export default function CellPowered() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black grain">
       <main className="mx-auto max-w-4xl px-4 py-10 min-h-screen bg-black">
         <Navbar />
         <Hero />
