8d1c4c08ea70b6c734816dccd740a200bbe932a3
noahgree <ngreene2026@gmail.com>
2026-01-09 03:47:10 -0500

    Update grain

--- src/components/cell-powered/Navbar.jsx
+++ src/components/cell-powered/Navbar.jsx
@@ -4,7 +4,7 @@ import { AppleIcon, DownloadIcon } from "../../Reusable/svgs";
 export default function Navbar() {
    return (
       <div className="flex fixed top-0 left-0 right-0 z-50 justify-center max-w-4xl mx-auto">
-         <div className="navbar backdrop-blur-2xl flex-nowrap rounded-4xl overflow-hidden w-[80vw] min-w-60 my-8 grain-bg [--grain-opacity:0.2] border border-white/20">
+         <div className="navbar backdrop-blur-2xl flex-nowrap rounded-4xl overflow-hidden w-[80vw] min-w-60 my-8 grain-bg border border-white/20">
             <div className="navbar-start z-1">
                <a
                href="#top"
