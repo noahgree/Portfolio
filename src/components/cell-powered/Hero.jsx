e89723003bcc4d9d1dbb67bf58512db4ed4b2ecd
noahgree <ngreene2026@gmail.com>
2026-01-09 03:38:18 -0500

    Update height again

--- src/components/cell-powered/Hero.jsx
+++ src/components/cell-powered/Hero.jsx
@@ -4,7 +4,7 @@ import { titleCN } from "../../Reusable/typography";
 
 export default function Hero() {
    return (
-      <div className="flex flex-col h-[75vh] items-center justify-center min-h-125 gap-8">
+      <div className="flex flex-col items-center justify-center h-[60vh] min-h-175 gap-8">
          <img src={DarkIcon}
          alt="Cell Powered Icon (dark)"
          className="h-24 rotate-6 drop-shadow-green-400/70 drop-shadow-xl mb-10" />
@@ -13,7 +13,7 @@ export default function Hero() {
             Cell Powered
          </h1>
          <h3 className="text-2xl opacity-70 text-center">
-            Keep an eye on your Mac's battery
+            Keep an eye on your MacBook's battery.
          </h3>
       </div>
    );
